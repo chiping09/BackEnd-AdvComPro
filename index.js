5459de9710e4131579edc81319bd11ddf0d30548
chiping09 <chip__ing@hotmail.com>
2024-09-11 10:48:46 +0700

    Week9

--- index.js
+++ index.js
@@ -1,14 +1,90 @@
-require("dotenv").config();
-
-const express = require("express");
+const express = require('express');
+const Sequelize = require('sequelize');
 const app = express();
-const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hallo World!");
+app.use(express.json());
+
+const dbUrl = 'postgres://webadmin:VBFtry51177@node65829-chip-app.proen.app.ruk-com.cloud/Books'
+
+const sequelize = new Sequelize(dbUrl);
+
+const Book = sequelize.define('book',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    author: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+sequelize.sync();
+
+app.get('/books', (req,res) => {
+    Book.findAll().then(books => {
+        res.json(books);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
 });
-  
+
+app.get('/books/:id', (req,res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('Book not found');
+        } else {
+            res.json(book);
+        }
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.post('/books', (req,res) => {
+    Book.create(req.body).then(book => {
+        res.send(book);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.put('/books/:id', (req,res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('Book not found');
+        } else {
+            book.update(req.body).then(() => {
+                res.send(book);
+            }).catch(err => {
+                res.status(500).send(err);
+            });
+        }
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.delete('/books/:id', (req,res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('Book not found');
+        } else {
+            book.destroy().then(() => {
+                res.send({});
+            }).catch(err => {
+                res.status(500).send(err);
+            });
+        }
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
--- src/PGSqlCRUD.js
+++ src/PGSqlCRUD.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const dbUrl = ''
+const dbUrl = 'postgres://webadmin:VBFtry51177@node65829-chip-app.proen.app.ruk-com.cloud:11248/Books'
 
 const sequelize = new Sequelize(dbUrl);
 
